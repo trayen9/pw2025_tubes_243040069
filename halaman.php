b8da16b3ac6380e7632312e22185f58801d3eebb
trayen9 <grahadestrian9@gmail.com>
2025-06-07 14:12:05 +0700

    commit

--- halaman.php
+++ halaman.php
@@ -7,6 +7,8 @@
     <title>DreamJob - Job Portal</title>
     <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 </head>
 
 <body>
@@ -80,23 +82,16 @@
 
             <div class="filter-buttons">
                 <select class="filter-btn">
-                    <option>Level</option>
-                    <option>Entry Level</option>
-                    <option>Mid Level</option>
-                    <option>Senior Level</option>
-                </select>
-                <select class="filter-btn">
                     <option>Jenis</option>
                     <option>Full Time</option>
                     <option>Part Time</option>
-                    <option>Contract</option>
-                    <option>Internship</option>
+                    <option>Magang</option>
+                    <option>Freelance</option>
                 </select>
                 <select class="filter-btn">
                     <option>Tipe</option>
                     <option>On-site</option>
                     <option>Remote</option>
-                    <option>Hybrid</option>
                 </select>
                 <select class="filter-btn">
                     <option>Lokasi</option>
@@ -111,9 +106,16 @@
                     <option>5-10 Juta</option>
                     <option>> 10 Juta</option>
                 </select>
+                <select class="filter-btn">
+                    <option>Pendidikan</option>
+                    <option>SMA / SMK / STM</option>
+                    <option>Diploma / D3 / D4</option>
+                    <option>Sarjana / S1</option>
+                </select>
             </div>
         </div>
 
+        <h1 class="section-title">Bidang Pekerjaan Terpopuler</h1>
         <div class="job-categories">
             <a href="#" class="job-category">Semua Pekerjaan</a>
             <a href="#" class="job-category">Data & Product</a>
@@ -127,6 +129,61 @@
             <a href="#" class="job-category">Health & Science</a>
         </div>
     </main>
+
+    <div class="container">
+        <div class="content-section">
+            <h1>Temukan pekerjaan yang diinginkan</h1>
+            <p>Dapatkan kemudahan akses ke berbagai daftar pekerjaan, dan informasi tentang lowongan pekerjaan terbaru.</p>
+            <button class="search-button">Cari Lowongan Sekarang</button>
+        </div>
+
+        <div class="hero-image">
+            <img src="./image/foto.png" alt="">
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-col description">
+                <h2><span style="color: #7f32ff">Dream</span><span style="color: #ffcc00">Job</span></h2>
+                <p>Website Lowongan Kerja dan Loker Terbaru di Indonesia</p>
+                <p>
+                    DreamJob adalah job portal dan website cari kerja di Indonesia dengan lowongan kerja berkualitas dari perusahaan terbaik.<br>
+                </p>
+                <p>Dapatkan kesempatan pekerjaan baru & belajar dari Mentor terbaik di Indonesia</p>
+
+            </div>
+
+            <div class="footer-right">
+
+                <div class="footer-col">
+                    <h4>Loker</h4>
+                    <ul>
+                        <li>Loker Penuh Waktu</li>
+                        <li>Loker Paruh Waktu</li>
+                        <li>Loker Magang</li>
+                        <li>Loker Freelance</li>
+                    </ul>
+                </div>
+
+
+                <div class="footer-col">
+                    <h4>Hubungi Kami</h4>
+                    <p>Jl. Dr. Setiabudi No.193, Gegerkalong, Kec. Sukasari, Kota Bandung, Jawa Barat 40153</p>
+
+                    <div class="social-icons">
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>© 2025 <strong>DreamJob.</strong> All rights reserved</p>
+        </div>
+    </footer>
 </body>
 
 </html>
